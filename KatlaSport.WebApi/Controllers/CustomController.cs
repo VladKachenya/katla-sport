4b70a15550580617256c9e8b7e26448776485d40
Vlad Kachenia <Uladzislau_Kachenia@epam.com>
2019-07-27 21:12:12 +0300

    Test CI/CD

---
+++ KatlaSport.WebApi/Controllers/CustomController.cs
@@ -0,0 +1,25 @@
+﻿using System.Net;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using KatlaSport.WebApi.CustomFilters;
+using Microsoft.Web.Http;
+using Swashbuckle.Swagger.Annotations;
+
+namespace KatlaSport.WebApi.Controllers
+{
+    [ApiVersion("1.0")]
+    [RoutePrefix("api/Custom")]
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    [CustomExceptionFilter]
+    [SwaggerResponseRemoveDefaults]
+    public class CustomController : ApiController
+    {
+        [HttpGet]
+        [Route("")]
+        [SwaggerResponse(HttpStatusCode.InternalServerError)]
+        public IHttpActionResult GetHives()
+        {
+            return Ok("Hello world!");
+        }
+    }
+}
