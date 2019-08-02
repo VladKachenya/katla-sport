2f214310512b9124651c5348a441acbbe57b6fb0
Vlad Kachenia <Uladzislau_Kachenia@epam.com>
2019-08-02 12:43:40 +0300

    Replace sync. method calls in HivesController and HiveService with async.

--- KatlaSport.Services.Models/HiveManagement/IHiveService.cs
+++ KatlaSport.Services.Models/HiveManagement/IHiveService.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace KatlaSport.Services.HiveManagement
 {
@@ -8,44 +9,46 @@ namespace KatlaSport.Services.HiveManagement
     public interface IHiveService
     {
         /// <summary>
-        /// Gets a hives list.
+        /// Gets a hives list async.
         /// </summary>
-        /// <returns>A <see cref="List{HiveListItem}"/>.</returns>
-        List<HiveListItem> GetHives();
+        /// <returns>A <see cref="Task{List{HiveListItem}}"/>.</returns>
+        Task<List<HiveListItem>> GetHivesAsync();
 
         /// <summary>
-        /// Gets a hive with specified identifier.
+        /// Gets a hive with specified identifier async.
         /// </summary>
         /// <param name="hiveId">A hive identifier.</param>
-        /// <returns>A <see cref="Hive"/>.</returns>
-        Hive GetHive(int hiveId);
+        /// <returns>A <see cref="Task{Hive}"/>.</returns>
+        Task<Hive> GetHiveAsync(int hiveId);
 
         /// <summary>
-        /// Creates a new hive.
+        /// Creates a new hive async.
         /// </summary>
         /// <param name="createRequest">A <see cref="UpdateHiveRequest"/>.</param>
-        /// <returns>A <see cref="Hive"/>.</returns>
-        Hive CreateHive(UpdateHiveRequest createRequest);
+        /// <returns>A <see cref="Task{Hive}"/>.</returns>
+        Task<Hive> CreateHiveAsync(UpdateHiveRequest createRequest);
 
         /// <summary>
-        /// Updates an existed hive.
+        /// Updates an existed hive async.
         /// </summary>
         /// <param name="hiveId">A hive identifier.</param>
         /// <param name="updateRequest">A <see cref="UpdateHiveRequest"/>.</param>
-        /// <returns>A <see cref="Hive"/>.</returns>
-        Hive UpdateHive(int hiveId, UpdateHiveRequest updateRequest);
+        /// <returns>A <see cref="Task{Hive}"/>.</returns>
+        Task<Hive> UpdateHiveAsync(int hiveId, UpdateHiveRequest updateRequest);
 
         /// <summary>
-        /// Deletes an existed hive.
+        /// Deletes an existed hive async.
         /// </summary>
         /// <param name="hiveId">A hive identifier.</param>
-        void DeleteHive(int hiveId);
+        /// <returns>A <see cref="Task"/>.</returns>
+        Task DeleteHiveAsync(int hiveId);
 
         /// <summary>
-        /// Sets deleted status for a hive.
+        /// Sets deleted status for a hive async.
         /// </summary>
         /// <param name="hiveId">A hive identifier.</param>
         /// <param name="deletedStatus">Status.</param>
-        void SetStatus(int hiveId, bool deletedStatus);
+        /// <returns>A <see cref="Task"/>.</returns>
+        Task SetStatusAsync(int hiveId, bool deletedStatus);
     }
 }
